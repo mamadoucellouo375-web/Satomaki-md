8426d3c0cf2b8c3aff951ca80e21448d3dc77a99
mamadoucellouo375-web <mamadoucellouo375@gmail.com>
2026-08-03 06:43:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ process.on('unhandledRejection', (reason) => {
 
 import connectToWhatsapp from './Digix/crew.js'
 import handleIncomingMessage from './events/messageHandler.js'
+import { startTempCleaner } from './utils/tempCleaner.js'
+
+startTempCleaner()
 
 (async() => {
     await connectToWhatsapp(handleIncomingMessage)
         console.log('established !')
 })()
-
